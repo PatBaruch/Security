3fe82e9c6cfd2e065601ad9a40e1c4a4f4451e36
PatBaruch <baru0001@hz.nl>
2024-06-16 18:56:01 +0200

    Security1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+Auth::routes();
+
+// Group routes that should only be accessible to authenticated users
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    // Add other routes that require authentication here
+    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 });
